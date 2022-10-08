de939605fb33c1f111644bf4be77bdd6b7deec7a
Daniel Leal <cosaleal@gmail.com>
2022-10-08 16:08:58 -0300

    Add support for URLs passed to the CLI

--- receipt_scanner/image/utils.py
+++ receipt_scanner/image/utils.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 import cv2
+import httpx
 import numpy as np
 
 from receipt_scanner.image.debug import debug_show
@@ -8,11 +9,9 @@ from receipt_scanner.image.debug import debug_show
 logger = getLogger(__name__)
 
 
-def open_image(file_name: str, debug: bool = False) -> np.ndarray:
+def open_image(image_location: str, debug: bool = False) -> np.ndarray:
     logger.debug("Opening image...")
-    original = cv2.imread(file_name)
-    if original is None:
-        raise FileNotFoundError(f"Couldn't find file {file_name}")
+    original = fetch_image(image_location)
     bordered_image = cv2.copyMakeBorder(original, 50, 50, 50, 50, cv2.BORDER_CONSTANT)
     debug_show(bordered_image, debug=debug)
     return bordered_image
@@ -21,3 +20,15 @@ def open_image(file_name: str, debug: bool = False) -> np.ndarray:
 def get_ratio_for_width(image: np.ndarray, target_width: int) -> float:
     image_width = image.shape[1]
     return target_width / image_width
+
+
+def fetch_image(image_location: str) -> np.ndarray:
+    try:
+        response = httpx.get(image_location)
+        image_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+        image = cv2.imdecode(image_array, -1)
+    except (cv2.error, httpx.UnsupportedProtocol):
+        image = cv2.imread(image_location)
+    if image is None:
+        raise FileNotFoundError(f"Couldn't find image located at '{image_location}'")
+    return image
