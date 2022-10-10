d66d39a8d4f4f67f731b37d7dced716dda78b2b4
Daniel Leal <cosaleal@gmail.com>
2022-10-10 18:27:13 -0300

    Change the default charset specification

--- receipt_scanner/image_to_text.py
+++ receipt_scanner/image_to_text.py
@@ -7,7 +7,7 @@ logger = getLogger(__name__)
 
 
 ALLOWED_CHARACTERS = (
-    "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz1234567890\ .#+-:"
+    "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz1234567890\ "
 )
 
 
