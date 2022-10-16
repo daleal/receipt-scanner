29f2838d0abb766f5d36683ffc8a6c0b16e8aed5
Daniel Leal <cosaleal@gmail.com>
2022-10-16 18:15:44 -0300

    Remove compression visualization as it makes no sense

--- receipt_scanner/image/filters/compress.py
+++ receipt_scanner/image/filters/compress.py
@@ -3,19 +3,13 @@ from logging import getLogger
 import cv2
 import numpy as np
 
-from receipt_scanner.image.debug import debug_show
 from receipt_scanner.image.filters.base_filter import Filter
 
 logger = getLogger(__name__)
 
 
 class CompressFilter(Filter):
-    def __init__(self, debug: bool = False) -> None:
-        self.debug = debug
-
     def eval(self, image: np.ndarray) -> np.ndarray:
         logger.debug("Applying 'CompressFilter' with automatic ratio...")
         _, encoded_image = cv2.imencode(".jpg", image)
-        compressed_image = cv2.imdecode(encoded_image, 1)
-        debug_show(compressed_image, debug=self.debug)
-        return compressed_image
+        return cv2.imdecode(encoded_image, 1)
