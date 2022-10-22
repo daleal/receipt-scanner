9760e55fb94fe6ac398682a649dfc246e0375b3f
Daniel Leal <cosaleal@gmail.com>
2022-10-21 23:17:15 -0300

    Use a smaller gaussian blur for the contour detection

--- receipt_scanner/image/filters/gaussian_blur.py
+++ receipt_scanner/image/filters/gaussian_blur.py
@@ -10,11 +10,12 @@ logger = getLogger(__name__)
 
 
 class GaussianBlurFilter(Filter):
-    def __init__(self, debug: bool = False) -> None:
+    def __init__(self, size: int = 7, debug: bool = False) -> None:
+        self.size = size
         self.debug = debug
 
     def eval(self, image: np.ndarray) -> np.ndarray:
-        logger.debug("Applying 'GaussianBlurFilter'...")
-        blurred_image = cv2.GaussianBlur(image, (7, 7), 0)
+        logger.debug(f"Applying 'GaussianBlurFilter' with size {self.size}...")
+        blurred_image = cv2.GaussianBlur(image, (self.size, self.size), 0)
         debug_show(blurred_image, debug=self.debug)
         return blurred_image
